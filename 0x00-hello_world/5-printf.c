3dc48a11a8f5dc3ac9e734e3ac87241c7b7c8df2
wildabyss37 <idibe28@gmail.com>
2022-09-10 10:39:46 -0700

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - Prints using printf()
+ *Return: 0 to end program
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
