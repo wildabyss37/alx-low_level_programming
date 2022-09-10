83881632cfe2d80f729a1863f1f4587dcb751758
wildabyss37 <idibe28@gmail.com>
2022-09-10 10:35:00 -0700

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - string using puts() function
+ *Return: 0 to indicate end of program
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
